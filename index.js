3b5da22e7fda235eb23920f15607c530f775ed20
Jayper Sanchez <jaypersanchez@gmail.com>
2025-04-10 07:00:20 +0800

    removed instagram and twitter signin and prompts user if login has failed from loginform.

--- index.js
+++ index.js
@@ -8,13 +8,14 @@ require('dotenv').config();
 // Middleware to parse JSON requests
 app.use(cors());
 app.use(express.json());
-const baseUrl = `${process.env.SCOUTJAR_SERVER_BASE_URL}${process.env.SCOUTJAR_SERVER_BASE_PORT}`;
+const baseUrl = `${process.env.SCOUTJAR_SERVER_BASE_URL}:${process.env.SCOUTJAR_SERVER_BASE_PORT}`;
 
 //const AIbaseURL = `${import.meta.env.VITE_SCOUTJAR_AI_BASE_URL}${import.meta.env.VITE_SCOUTJAR_AI_BASE_PORT}`;
 
 // Endpoint to test the database connection
 app.get('/dbtest', async (req, res) => {
     try {
+      console.log(pool)
       const result = await pool.query('SELECT NOW()');
       res.json({
         message: 'Database connection successful!',
--- start-dev.sh
+++ start-dev.sh
@@ -1,1 +1,20 @@
-npm run start:dev
+#!/bin/bash
+
+# Set up Node version if using nvm (optional if already set)
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+# Go to project
+cd ~/projects/scoutjar/scoutjar-server
+
+# Install dependencies if needed
+npm install
+
+# Stop any existing scoutjar-server process
+pm2 delete scoutjar-server || true
+
+# Start new scoutjar-server
+pm2 start npm --name "scoutjar-server" -- run start:dev
+
+# Save the pm2 process list
+pm2 save
