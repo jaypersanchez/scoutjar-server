eb64e059791ccdd25370b0cb169d3abf967f031e
Jayper Sanchez <jaypersanchez@gmail.com>
2025-04-29 11:09:16 +0800

    changed on startup script

--- start-dev.sh
+++ start-dev.sh
@@ -7,11 +7,15 @@ export NVM_DIR="$HOME/.nvm"
 # Go to project
 cd ~/projects/scoutjar/scoutjar-server
 
+# Pull latest code
+git fetch origin
+git reset --hard origin/mvp0.1 
+
 # Install dependencies if needed
 npm install
 
 # Stop any existing scoutjar-server process
-pm2 delete scoutjar-server || true
+pm2 delete scoutjar-server-mvp0.1 || true
 
 # Start new scoutjar-server
 pm2 start npm --name "scoutjar-server-mvp0.1" -- run start:dev
