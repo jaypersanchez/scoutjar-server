7a2f3e2ddac1710ed5facf84e532c56e1f4e8330
Jayper Sanchez <jaypersanchez@gmail.com>
2025-07-12 16:18:00 +0800

    added get job titles and job categories end points

--- routes/jobTitles.js
+++ routes/jobTitles.js
@@ -5,7 +5,7 @@ const pool = require('../db');
 // Get all job titles (or with optional query param for filtering)
 router.get('/job-titles', async (req, res) => {
   const { q } = req.query;
-  const titles = await db.query(`
+  const titles = await pool.query(`
     SELECT DISTINCT job_title 
     FROM job_titles 
     WHERE job_title ILIKE $1
@@ -14,10 +14,9 @@ router.get('/job-titles', async (req, res) => {
   res.json(titles.rows.map(row => row.job_title));
 });
 
-// Get unique job categories
 router.get('/job-categories', async (req, res) => {
   const { q } = req.query;
-  const categories = await db.query(`
+  const categories = await pool.query(`
     SELECT DISTINCT job_category 
     FROM job_titles 
     WHERE job_category ILIKE $1
